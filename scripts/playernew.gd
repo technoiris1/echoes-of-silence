e76f1b818bc35cf3fac8dbc041983139534fae6d
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-20 09:47:05 +0530

    fixed the camera pause on esc

--- scripts/playernew.gd
+++ scripts/playernew.gd
@@ -1,37 +1,43 @@
 extends CharacterBody3D
+
 @export var walk_speed := 7.0
 @export var sprint_speed := 12.0
 @export var jump_strength := 20.0
 @export var gravity := 50.0
 
-
 @onready var spring_arm: SpringArm3D = $SpringArm3D
 @onready var model: Node3D = $character
 @onready var animation_player: AnimationPlayer = $character/AnimationPlayer
 
-
-var game_active := true
 var is_sprinting := false
-
+var is_paused := false
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	animation_player.play("idle")
 
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		game_active = !game_active
-		Input.mouse_mode = (
-			Input.MOUSE_MODE_CAPTURED
-			if game_active
-			else Input.MOUSE_MODE_VISIBLE
-		)
+		toggle_pause()
+		return
+	if is_paused:
+		return
 
+func toggle_pause() -> void:
+	is_paused = !is_paused
+	get_tree().paused = is_paused
+
+	Input.mouse_mode = (
+		Input.MOUSE_MODE_VISIBLE
+		if is_paused
+		else Input.MOUSE_MODE_CAPTURED
+	)
 
 func _physics_process(delta: float) -> void:
-	if not game_active:
+	if is_paused:
 		return
+
 	var input_dir := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("back") - Input.get_action_strength("front")
@@ -42,10 +48,8 @@ func _physics_process(delta: float) -> void:
 	move_dir.y = 0
 	move_dir = move_dir.normalized()
 
-
 	is_sprinting = Input.is_action_pressed("sprint")
-	var current_speed := sprint_speed if is_sprinting else walk_speed
-
+	var speed := sprint_speed if is_sprinting else walk_speed
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -54,9 +58,8 @@ func _physics_process(delta: float) -> void:
 		velocity.y = jump_strength
 		animation_player.play("running_jump")
 
-
-	velocity.x = move_dir.x * current_speed
-	velocity.z = move_dir.z * current_speed
+	velocity.x = move_dir.x * speed
+	velocity.z = move_dir.z * speed
 
 	move_and_slide()
 
@@ -64,17 +67,13 @@ func _physics_process(delta: float) -> void:
 		model.rotation.y = atan2(velocity.x, velocity.z)
 
 	if not is_on_floor():
-		pass 
+		pass
 	elif move_dir.length() > 0.1:
-		if is_sprinting:
-			if animation_player.current_animation != "running":
-				animation_player.play("running")
-		else:
-			if animation_player.current_animation != "walking":
-				animation_player.play("walking")
+		animation_player.play("running" if is_sprinting else "walking")
 	else:
-		if animation_player.current_animation != "idle":
-			animation_player.play("idle")
+		animation_player.play("idle")
 
 func _process(_delta: float) -> void:
+	if is_paused:
+		return
 	spring_arm.global_position = global_position
--- scripts/spring_arm_3d.gd
+++ scripts/spring_arm_3d.gd
@@ -1,14 +1,18 @@
 extends SpringArm3D
+
 @export var mouse_sensitivity := 0.05
 
 func _ready() -> void:
-	# Remove set_as_top_level(true)
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event: InputEvent) -> void:
+	if get_tree().paused:
+		return
+
 	if event is InputEventMouseMotion:
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
 		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
-		
+
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
