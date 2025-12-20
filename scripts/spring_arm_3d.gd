3c260897af8da28e184c29e7a04667f4e3b1afb4
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-20 10:20:20 +0530

    fixed the player camera issues

--- scripts/spring_arm_3d.gd
+++ scripts/spring_arm_3d.gd
@@ -12,7 +12,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if event is InputEventMouseMotion:
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
+		rotation_degrees.x = clamp(rotation_degrees.x, -75.0, 5.0)
 
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
