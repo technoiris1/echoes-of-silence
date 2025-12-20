02c1ba368692e4cbd66698dc2b7bbe82f5134aa8
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-20 08:59:09 +0530

    added run and jump animations, kinda broken rn tho

--- scripts/playernew.gd
+++ scripts/playernew.gd
@@ -1,49 +1,80 @@
 extends CharacterBody3D
-@export var speed := 7.0
+@export var walk_speed := 7.0
+@export var sprint_speed := 12.0
 @export var jump_strength := 20.0
 @export var gravity := 50.0
-@onready var _spring_arm: SpringArm3D = $SpringArm3D
-@onready var _model: Node3D = $character
-@onready var animation_player = $character/AnimationPlayer
+
+
+@onready var spring_arm: SpringArm3D = $SpringArm3D
+@onready var model: Node3D = $character
+@onready var animation_player: AnimationPlayer = $character/AnimationPlayer
+
+
+var game_active := true
+var is_sprinting := false
+
 
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	animation_player.play("idle")
 
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		game_active = !game_active
+		Input.mouse_mode = (
+			Input.MOUSE_MODE_CAPTURED
+			if game_active
+			else Input.MOUSE_MODE_VISIBLE
+		)
+
+
 func _physics_process(delta: float) -> void:
-	# Get input relative to camera
-	var input_dir := Vector2.ZERO
-	input_dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	input_dir.y = Input.get_action_strength("back") - Input.get_action_strength("front")
-	input_dir = input_dir.normalized()
-	
-	# Transform input direction based on camera rotation
-	var cam_basis = _spring_arm.global_transform.basis
-	var move_direction = (cam_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	move_direction.y = 0  # Keep movement on horizontal plane
-	
+	if not game_active:
+		return
+	var input_dir := Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("back") - Input.get_action_strength("front")
+	).normalized()
+
+	var cam_basis := spring_arm.global_transform.basis
+	var move_dir := cam_basis * Vector3(input_dir.x, 0, input_dir.y)
+	move_dir.y = 0
+	move_dir = move_dir.normalized()
+
+
+	is_sprinting = Input.is_action_pressed("sprint")
+	var current_speed := sprint_speed if is_sprinting else walk_speed
+
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_strength
-	
-	velocity.x = move_direction.x * speed
-	velocity.z = move_direction.z * speed
-	
+		animation_player.play("running_jump")
+
+
+	velocity.x = move_dir.x * current_speed
+	velocity.z = move_dir.z * current_speed
+
 	move_and_slide()
-	
-	# Handle rotation and animations
-	if velocity.length() > 0.2:
-		# Make character face movement direction
-		_model.rotation.y = atan2(velocity.x, velocity.z)
-		
-		# Play walking animation when moving
-		if animation_player.current_animation != "walking":
-			animation_player.play("walking")
+
+	if move_dir.length() > 0.1:
+		model.rotation.y = atan2(velocity.x, velocity.z)
+
+	if not is_on_floor():
+		pass 
+	elif move_dir.length() > 0.1:
+		if is_sprinting:
+			if animation_player.current_animation != "running":
+				animation_player.play("running")
+		else:
+			if animation_player.current_animation != "walking":
+				animation_player.play("walking")
 	else:
-		# Play idle animation when not moving
 		if animation_player.current_animation != "idle":
 			animation_player.play("idle")
-		
+
 func _process(_delta: float) -> void:
-	_spring_arm.global_position = global_position
+	spring_arm.global_position = global_position
