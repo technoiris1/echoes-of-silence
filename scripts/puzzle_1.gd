63747612384eb137c452f383b61c9aaa84a84153
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-27 22:39:50 +0530

    made the exit button consistent

--- scripts/puzzle_1.gd
+++ scripts/puzzle_1.gd
@@ -25,19 +25,23 @@ var chosen_symbols: Array[String] = []
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	exit_button.visible = true
+
 	for child in buttons_container.get_children():
 		if child is TextureButton:
 			child.pressed.connect(_on_symbol_pressed.bind(child))
 
 	clear_slots()
 	message_label.text = ""
+
 func _on_symbol_pressed(button: TextureButton) -> void:
 	if current_slot >= 4:
 		return
 
 	var symbol_name := button.name
 	var raw_texture := button.texture_normal
-	var fixed_texture := resized_texture(raw_texture, ICON_SIZE)
+	var fixed_texture := raw_texture
 
 	slots[current_slot].texture = fixed_texture
 	chosen_symbols.append(symbol_name)
@@ -51,7 +55,6 @@ func _on_submit_pressed() -> void:
 
 	if chosen_symbols == CORRECT_SEQUENCE:
 		show_message("Correct sequence!", Color.GREEN)
-		exit_button.visible = true
 	else:
 		show_message("Wrong sequence. Try again.", Color.RED)
 		clear_slots()
@@ -66,16 +69,9 @@ func clear_slots() -> void:
 
 	chosen_symbols.clear()
 	current_slot = 0
-	exit_button.visible = false
-
 func _on_exit_pressed() -> void:
 	queue_free()
 
 func show_message(text: String, color: Color) -> void:
 	message_label.text = text
 	message_label.modulate = color
-
-func resized_texture(tex: Texture2D, size: Vector2i) -> Texture2D:
-	var img := tex.get_image()
-	img.resize(size.x, size.y, Image.INTERPOLATE_LANCZOS)
-	return ImageTexture.create_from_image(img)
