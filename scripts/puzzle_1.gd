61f9fe6f2aba9b4d43c043f981754ad304c2a5fc
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-28 00:37:24 +0530

    connected the puzzle to the main game

--- scripts/puzzle_1.gd
+++ scripts/puzzle_1.gd
@@ -1,6 +1,7 @@
 extends Control
 
-const ICON_SIZE := Vector2i(64, 64)
+signal puzzle_solved
+signal puzzle_closed
 
 const CORRECT_SEQUENCE := [
 	"circle",
@@ -22,30 +23,23 @@ const CORRECT_SEQUENCE := [
 
 var current_slot := 0
 var chosen_symbols: Array[String] = []
+var solved := false
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-	exit_button.visible = true
 
 	for child in buttons_container.get_children():
 		if child is TextureButton:
 			child.pressed.connect(_on_symbol_pressed.bind(child))
 
 	clear_slots()
-	message_label.text = ""
 
 func _on_symbol_pressed(button: TextureButton) -> void:
 	if current_slot >= 4:
 		return
 
-	var symbol_name := button.name
-	var raw_texture := button.texture_normal
-	var fixed_texture := raw_texture
-
-	slots[current_slot].texture = fixed_texture
-	chosen_symbols.append(symbol_name)
-
+	slots[current_slot].texture = button.texture_normal
+	chosen_symbols.append(button.name)
 	current_slot += 1
 
 func _on_submit_pressed() -> void:
@@ -55,6 +49,7 @@ func _on_submit_pressed() -> void:
 
 	if chosen_symbols == CORRECT_SEQUENCE:
 		show_message("Correct sequence!", Color.GREEN)
+		solved = true
 	else:
 		show_message("Wrong sequence. Try again.", Color.RED)
 		clear_slots()
@@ -66,10 +61,14 @@ func _on_clear_pressed() -> void:
 func clear_slots() -> void:
 	for slot in slots:
 		slot.texture = null
-
 	chosen_symbols.clear()
 	current_slot = 0
+
 func _on_exit_pressed() -> void:
+	if solved:
+		emit_signal("puzzle_solved")
+	else:
+		emit_signal("puzzle_closed")
 	queue_free()
 
 func show_message(text: String, color: Color) -> void:
---
+++ scripts/roof_gate.gd
@@ -0,0 +1,60 @@
+extends Node
+
+@export var puzzle_scene: PackedScene
+
+@onready var gate_mesh := $door
+@onready var gate_collision := $collisions/collisionbody
+@onready var gate_area := $area
+@onready var UILayer := $Ui_Layer
+var player_inside := false
+var solved := false
+var puzzle_instance: Control = null
+
+func _ready() -> void:
+	gate_area.body_entered.connect(_on_body_entered)
+	gate_area.body_exited.connect(_on_body_exited)
+
+func _process(_delta: float) -> void:
+	if player_inside and not solved and Input.is_action_just_pressed("interact"):
+		print("e pressed")
+		open_puzzle()
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "player":
+		player_inside = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.name == "player":
+		player_inside = false
+
+func open_puzzle() -> void:
+	if puzzle_instance != null:
+		return
+
+	get_tree().paused = true
+
+	puzzle_instance = puzzle_scene.instantiate()
+	puzzle_instance.process_mode = Node.PROCESS_MODE_ALWAYS
+
+	puzzle_instance.puzzle_solved.connect(_on_puzzle_solved)
+	puzzle_instance.puzzle_closed.connect(_on_puzzle_closed)
+
+	UILayer.add_child(puzzle_instance)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_puzzle_solved() -> void:
+	solved = true
+	close_puzzle()
+	open_gate()
+
+func _on_puzzle_closed() -> void:
+	close_puzzle()
+
+func close_puzzle() -> void:
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	puzzle_instance = null
+
+func open_gate() -> void:
+	gate_mesh.visible = false
+	gate_collision.disabled = true
