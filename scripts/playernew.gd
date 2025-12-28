894d6ecb4b1fd7cb12682350015899e0816c917c
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-28 13:54:42 +0530

    game was not resuming once paused with esc. so fixed it

--- scripts/playernew.gd
+++ scripts/playernew.gd
@@ -12,16 +12,13 @@ extends CharacterBody3D
 var is_sprinting := false
 
 func _ready() -> void:
-
-	process_mode = Node.PROCESS_MODE_PAUSABLE
-
+	process_mode = Node.PROCESS_MODE_ALWAYS  
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	animation_player.play("idle")
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("escape"):
+	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().paused = !get_tree().paused
-
 		Input.mouse_mode = (
 			Input.MOUSE_MODE_VISIBLE
 			if get_tree().paused
@@ -29,34 +26,37 @@ func _unhandled_input(event: InputEvent) -> void:
 		)
 
 func _physics_process(delta: float) -> void:
+	if get_tree().paused:
+		return  
+	
 	var input_dir := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("back") - Input.get_action_strength("front")
 	).normalized()
-
+	
 	var cam_basis := spring_arm.global_transform.basis
 	var move_dir := cam_basis * Vector3(input_dir.x, 0, input_dir.y)
 	move_dir.y = 0
 	move_dir = move_dir.normalized()
-
+	
 	is_sprinting = Input.is_action_pressed("sprint")
 	var speed := sprint_speed if is_sprinting else walk_speed
-
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_strength
 		animation_player.play("running_jump")
-
+	
 	velocity.x = move_dir.x * speed
 	velocity.z = move_dir.z * speed
-
+	
 	move_and_slide()
-
+	
 	if move_dir.length() > 0.1:
 		model.rotation.y = atan2(velocity.x, velocity.z)
-
+	
 	if not is_on_floor():
 		pass
 	elif move_dir.length() > 0.1:
@@ -65,4 +65,6 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("idle")
 
 func _process(_delta: float) -> void:
+	if get_tree().paused:
+		return 
 	spring_arm.global_position = global_position
