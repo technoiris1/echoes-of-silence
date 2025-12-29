66e352756d272dd23eead934e9c42db5b1a4d8b1
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-30 02:39:39 +0530

    added some rotating tiles to the parkour

--- scripts/first_step.gd
+++ scripts/first_step.gd
@@ -1,15 +1,19 @@
 extends Node
-
+# Rotation speed in radians per second
 @export var rotation_speed: float = 1.0
+@onready var mesh_instance = $MeshInstance3D
+@onready var collision_body = $StaticBody3D/CollisionShape3D
 
-@onready var mesh_instance = $MeshInstance3D  
-@onready var collision_body = $StaticBody3D/CollisionShape3D  
-
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	pass
 
-func _process(delta: float) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	# Rotate the mesh
 	if mesh_instance:
 		mesh_instance.rotate_y(rotation_speed * delta)
+	
+	# Rotate the collision body
 	if collision_body:
 		collision_body.rotate_y(rotation_speed * delta)
