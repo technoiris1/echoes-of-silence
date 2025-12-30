be246dc0626147b7f3cf151c1150836bb8db42dd
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-31 00:09:28 +0530

    made a portal to the new dimension

---
+++ scripts/portal_area.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+var next_scene_path: String = "res://scenes/parkour.tscn"
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	print("body entered")
+	get_tree().change_scene_to_file(next_scene_path)
